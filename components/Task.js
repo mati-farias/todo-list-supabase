dcf3584f163640fb4b4ab2c3442b3717908a2675
mati-farias <mati.farias91@hotmail.com>
2023-07-19 16:05:25 +0200

    added uncheck button

--- components/Task.js
+++ components/Task.js
@@ -8,17 +8,27 @@ export default function Task({ task, toggleTask, deleteTask }) {
         </h2>
         <p className='text-sm text-gray-500'>{task.description}</p>
       </div>
-      <div className='flex flex-col w-full mt-4 sm:flex-row sm:space-x-2 sm:w-auto sm:mt-0'>
+      <div className='flex flex-col w-full mt-4 space-y-2 sm:flex-row sm:space-x-2 sm:w-auto sm:mt-0 sm:space-y-0'>
         <button
-          onClick={() => toggleTask(task.id, task.isComplete)}
+          onClick={() =>
+            !task.isComplete && toggleTask(task.id, task.isComplete)
+          }
           className={`p-2 border-2 rounded flex-grow text-center ${
             task.isComplete ? 'bg-green-500' : 'bg-yellow-500'
-          }`}>
+          }`}
+          disabled={task.isComplete}>
           {task.isComplete ? 'Done!' : 'Not done yet!'}
         </button>
+        {task.isComplete && (
+          <button
+            onClick={() => toggleTask(task.id, task.isComplete)}
+            className='flex-grow p-2 text-center bg-blue-500 border-2 rounded'>
+            Uncheck
+          </button>
+        )}
         <button
           onClick={() => deleteTask(task.id)}
-          className='flex-grow p-2 mt-2 text-center bg-red-500 border-2 rounded sm:mt-0'>
+          className='flex-grow p-2 text-center bg-red-500 border-2 rounded'>
           Delete
         </button>
       </div>
